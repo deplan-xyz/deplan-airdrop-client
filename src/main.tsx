ea719d5e38ae02b79307a89a2b0dc04716dd53b9
kirillgladkov <crosfire2011@gmail.com>
2024-03-30 17:44:01 +0300

    Add index.scss and update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+
+import router from './config/router'
 import Web3ModalProvider from './providers/web3modal'
 
 import './index.scss'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Web3ModalProvider>
-      <App />
+      <RouterProvider router={router} />
     </Web3ModalProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
