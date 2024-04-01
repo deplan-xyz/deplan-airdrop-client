14bf777741a41d180825fccecba8205983d11924
Anton Pashkovskyi <125904314+antonpashdev@users.noreply.github.com>
2024-04-01 18:17:37 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,8 +7,8 @@ const Home = () => {
     return (
         <div className={styles.container}>
             <img src={Airdropslogo} width={220} height={220} alt="DePlan Airdrops" />
-            <span className={styles.title}>Connect Solana Crypto Wallet</span>
-            <span className={styles.description}>Connect your Solana crypto wallet and check if you are eligible for $DPLN airdrop</span>
+            <span className={styles.title}>Connect Solana Wallet</span>
+            <span className={styles.description}>Connect your Solana wallet and check if you are eligible for $DPLN airdrop</span>
             <div className={styles.connectBtn}>
                 <LogInWithWalletButton mode="connect" size="md" />
             </div>
