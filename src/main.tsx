bff8964e8467700ccc07234cfcebb1366a3583d2
kirillgladkov <crosfire2011@gmail.com>
2024-05-01 00:59:10 +0300

    Remove Moralis initialization code from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import Moralis from 'moralis'
 
 import router from './config/router'
 import Web3ModalProvider from './providers/web3modal'
@@ -17,13 +16,6 @@ const queryClient = new QueryClient({
     }
   }
 });
-
-(async () => {
-  await Moralis.start({
-    apiKey: import.meta.env.VITE_MORALIS_API_KEY,
-  });
-  console.log('Moralis initialized');
-})()
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
