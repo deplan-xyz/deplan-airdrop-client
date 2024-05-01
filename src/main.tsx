17c7822caf37e05c2a6135f66554c5dc76276bdb
kirillgladkov <crosfire2011@gmail.com>
2024-05-01 19:39:25 +0300

    Add SnackbarProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { SnackbarProvider } from 'notistack'
+
 
 import router from './config/router'
 import Web3ModalProvider from './providers/web3modal'
@@ -19,6 +21,7 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <SnackbarProvider />
     <Web3ModalProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
