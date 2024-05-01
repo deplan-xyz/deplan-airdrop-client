2b3d11d3911b85dbed35414eb33928fc6cefc14f
kirillgladkov <crosfire2011@gmail.com>
2024-05-01 19:39:37 +0300

    Update ClaimButton component styles and functionality

--- src/components/ClaimButton/index.tsx
+++ src/components/ClaimButton/index.tsx
@@ -1,15 +1,20 @@
 
+import { useState } from 'react';
 import { Transaction } from '@solana/web3.js';
+import { enqueueSnackbar } from 'notistack'
 import { useWeb3ModalProvider } from '@web3modal/solana/react';
+
 import { claim, claimSend } from '../../api/api';
 import useEligibility from '../../hooks/useEligibility';
 import useWallet from '../../hooks/useWallet';
+
 import styles from './ClaimButton.module.scss';
 
 
 const ClaimButton = () => {
+    const [loading, setLoading] = useState(false);
     const { address } = useWallet();
-    const { tokenAmount, isEligible } = useEligibility();
+    const { tokenAmount, isEligible, refetchClaimData } = useEligibility();
     const { walletProvider, connection } = useWeb3ModalProvider();
     const isDisabled = false;
 
@@ -17,7 +22,9 @@ const ClaimButton = () => {
         return null;
     }
 
+
     const onClaim = async () => {
+        setLoading(true);
         try {
             if (!address) {
                 alert('Please connect your wallet');
@@ -37,20 +44,31 @@ const ClaimButton = () => {
 
             const transaction = await claim(address);
 
-            const signedTxn = await walletProvider.signTransaction(transaction)
-            .catch((error: unknown) => {
-                console.error('Error signing transaction', error);
+            const signedTxn = await walletProvider.signTransaction(transaction).catch(() => {
+                enqueueSnackbar({ message: 'Transaction canceled by user', variant: 'error' });
             });
-            
-            await claimSend(address, (signedTxn as Transaction).serialize().toString('base64'));
+
+            if (!signedTxn) {
+                throw new Error('No signed transaction');
+            }
+
+            await claimSend(address, (signedTxn as Transaction).serialize().toString('base64')).catch(() => {
+                enqueueSnackbar({ message: 'Claim failed. Try again', variant: 'error', action: () => <button className={styles.retryTextButton} onClick={onClaim}>Retry</button> });
+            });
+
+            enqueueSnackbar({ message: `Successfully claimed ${tokenAmount.toLocaleString()} $DPLN`, variant: 'success' });
+            refetchClaimData();
 
         } catch (error) {
             console.error('Error claiming', error);
+            enqueueSnackbar({ message: 'Claim failed', variant: 'error' });
+        } finally {
+            setLoading(false);
         }
 
     };
 
-    return isDisabled ?
+    return isDisabled || loading ?
         <div className={`${styles.button} ${styles.disabled}`}><span>Claim {tokenAmount.toLocaleString()} $DPLN</span></div> :
         <button onClick={onClaim} className={`${styles.button} ${styles.active}`}>Claim {tokenAmount.toLocaleString()} $DPLN</button>
 }
