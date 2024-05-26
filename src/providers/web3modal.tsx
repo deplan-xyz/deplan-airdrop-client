17f30405b829c371b72b1aea877d89b81e081e10
Anton Pashkovskyi <125904314+antonpashdev@users.noreply.github.com>
2024-05-26 21:08:49 +0200

    Update web3modal.tsx

--- src/providers/web3modal.tsx
+++ src/providers/web3modal.tsx
@@ -32,18 +32,6 @@ createWeb3Modal({
   },
   termsConditionsUrl: 'https://walletconnect.com/terms',
   privacyPolicyUrl: 'https://walletconnect.com/privacy',
-  customWallets: [
-    {
-      id: 'deplanWallet',
-      name: 'DePlan',
-      homepage: 'deplan.xyz',
-      image_url: 'https://assets.coingecko.com/coins/images/36429/large/deplan_token.png',
-      mobile_link: 'deplan://',
-      desktop_link: 'deplan://',
-      app_store: 'https://apps.apple.com/app/id6478705732',
-      play_store: 'https://play.google.com/store/apps/details?id=xyz.deplan.app',
-    },
-  ],
 });
 
 interface Web3ModalProviderProps {
